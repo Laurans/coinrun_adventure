1767140aec44ecdca60257174f95b1c2929657ca
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-10-08 20:56:21 +0200

    :whale: Add config to run docker environment

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+image_name="coinrun_adventure"
+container_name="coinrun_venv"
+if [ ! "$(docker ps -q -f name=$container_name)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=$container_name)" ]; then
+        if [ "$1" == "rm" ]; then
+            docker rm $container_name
+            docker run --gpus all -it -v $PWD:/home/$USER/workdir --name $container_name $image_name
+        else
+            docker start -i $container_name
+        fi
+
+    else
+        docker run --gpus all -it -v $PWD:/home/$USER/workdir --name $container_name $image_name
+    fi
+fi 
