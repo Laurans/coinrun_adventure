627c2dfb609b39bd9fa58b209eeffd41d2db3d7e
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-10-10 22:16:13 +0200

    :whale: Update docker environment

--- run.sh
+++ run.sh
@@ -6,12 +6,11 @@ if [ ! "$(docker ps -q -f name=$container_name)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=$container_name)" ]; then
         if [ "$1" == "rm" ]; then
             docker rm $container_name
-            docker run --gpus all -it -v $PWD:/home/$USER/workdir --name $container_name $image_name
         else
             docker start -i $container_name
         fi
 
     else
-        docker run --gpus all -it -v $PWD:/home/$USER/workdir --name $container_name $image_name
+        docker run --gpus all -it -v $PWD:/home/vscode/workdir --name $container_name $image_name
     fi
 fi 
