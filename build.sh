e7bca4c4c4a789b59450384a35b5a396f0cc9c2b
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-10-10 22:56:42 +0200

    :whale: Add qt5 lib + x11 example + xvfb in Dockerfile

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd devcontainer && docker build --build-arg NB_USER=$USER --build-arg NB_UID=$(id -u) --build-arg NB_GID=$(id -g) -t coinrun_adventure -f Dockerfile .
+cd env && docker build --build-arg NB_USER=$USER --build-arg NB_UID=$(id -u) --build-arg NB_GID=$(id -g) --build-arg NB_DISPLAY=$DISPLAY -t coinrun_adventure -f Dockerfile .
