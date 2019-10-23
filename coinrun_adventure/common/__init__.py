1647d802e09db2f4a2f852cb107e53b43925b7d6
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-10-23 16:04:04 +0200

    :art: Format code with black

--- coinrun_adventure/common/__init__.py
+++ coinrun_adventure/common/__init__.py
@@ -1,4 +1,5 @@
+# flake8: noqa F403
 from .data_structures import *  # noqa
 from .distributions import *  # noqa
 from .networks import *  # noqa
-from .utilities import *  # noqa
+from .wrappers import *
