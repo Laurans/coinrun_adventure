3105e0a0b7db07f72bd84d7bd3ae684314eeb8d1
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2019-10-23 16:01:40 +0200

    :fire: Remove mpi files + unused file

--- coinrun_adventure/common/utilities.py
+++
@@ -1,8 +0,0 @@
-
-def discount_with_dones(rewards, dones, gamma):
-    discounted = []
-    r = 0
-    for reward, done in zip(rewards[::-1], dones[::-1]):
-        r = reward + gamma * r * (1.0 - done)
-        discounted.append(r)
-    return discounted[::-1]
