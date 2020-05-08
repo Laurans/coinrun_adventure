4c1aa9cc08301b53856b817427232d1b33403206
Anaelle Laurans <16943368+Laurans@users.noreply.github.com>
2020-05-08 21:38:58 +0200

    :whale: Update dockerfile image and installation

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd env && docker build --build-arg NB_USER=$USER --build-arg NB_UID=$(id -u) --build-arg NB_GID=$(id -g) -t coinrun_adventure -f Dockerfile .
+cd env && poetry export -f requirements.txt > requirements.txt && docker build --build-arg NB_USER=$USER --build-arg NB_UID=$(id -u) --build-arg NB_GID=$(id -g) -t coinrun_adventure -f Dockerfile .
